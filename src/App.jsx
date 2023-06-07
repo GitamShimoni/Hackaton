c8b65a654471c03a3afccb3854822ac87c5af84b
RanRDT <129321063+RanRDT@users.noreply.github.com>
2023-06-07 22:26:36 +0300

    rebasing 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import { useState } from "react";
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PhotoCarousel from "./Components/PhotoCarousel/PhotoCarousel";
 import Login from "./Components/login/Login";
 import Layout from "./Components/Layout/Layout";
 import Aboutus from "./Components/about/Aboutus";
 
 import "./App.css";
-import UploadWidget from './Components/UploadPage/UploadWidget'
+import UploadWidget from "./Components/UploadPage/UploadWidget";
 import ProfilePage from "./Components/ProfilePage/ProfilePage";
 
 import Homepage from "./Components/homepage/Homepage";
@@ -19,16 +19,12 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           {/* <Route path="/upload" element={<UploadWidget />}/> */}
-          <Route path="/login" element={<Homepage />} />
           <Route path="/about" element={<Aboutus />} />
+          <Route path="/" element={<PhotoCarousel />} />
+          <Route path="/upload" element={<UploadWidget />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Route>
       </Routes>
-    <Routes>
-      <Route path="/" element={<PhotoCarousel />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/upload" element={<UploadWidget />} />
-      <Route path="/profile" element={<ProfilePage />} />
-    </Routes>
     </>
   );
 }
